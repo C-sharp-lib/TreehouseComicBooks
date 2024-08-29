f3367a89e9b2ca5c883d8e3e2711f3e48ed1c0e5
Kevin Stradtman <stradtkt22@gmail.com>
2024-08-29 02:04:48 -0400

    Comic Book First Commit

---
+++ Controllers/ComicBooksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        // GET: ComicBooks
+        public ActionResult Index()
+        {
+            ViewData["Title"] = "Comic Book Home";
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            ViewData["Title"] = "Comic Book Details";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return View();
+        }
+    }
+}
