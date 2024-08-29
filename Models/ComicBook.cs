cb535306e68a43e607a31a6029d82658f4659c8d
Kevin Stradtman <stradtkt22@gmail.com>
2024-08-29 02:55:21 -0400

    Added more readonly actions to ComicBook

--- Models/ComicBook.cs
+++ Models/ComicBook.cs
@@ -5,7 +5,23 @@
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public string DescriptionHtml { get; set; }
+        public string IssueNumber { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
